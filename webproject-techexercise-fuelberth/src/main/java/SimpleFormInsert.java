2cbdee313064509ef972e3d2f5ebdd79a78366f9
ifuelberth <ifuelberth@10.70.251.116>
2023-10-13 18:51:08 -0500

    add test display

---
+++ webproject-techexercise-fuelberth/src/main/java/SimpleFormInsert.java
@@ -0,0 +1,74 @@
+
+/**
+ * @file SimpleFormInsert.java
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SimpleFormInsert")
+public class SimpleFormInsert extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+
+   public SimpleFormInsert() {
+      super();
+   }
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      String booktitle = request.getParameter("title");
+      String author = request.getParameter("author");
+      String pub = request.getParameter("publisher");
+      String available = request.getParameter("available");
+
+      Connection connection = null;
+      String insertSql = " INSERT INTO libraryTable (id, title, author, publisher, available) values (default, ?, ?, ?, ?)";
+
+      try {
+         DBConnection.getDBConnection();
+         connection = DBConnection.connection;
+         PreparedStatement preparedStmt = connection.prepareStatement(insertSql);
+         preparedStmt.setString(1, booktitle);
+         preparedStmt.setString(2, author);
+         preparedStmt.setString(3, pub);
+         preparedStmt.setString(4, available);
+         preparedStmt.execute();
+         connection.close();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+
+      // Set response content type
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+      String title = "Insert Data to DB table";
+      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+      out.println(docType + //
+            "<html>\n" + //
+            "<head><title>" + title + "</title></head>\n" + //
+            "<body bgcolor=\"#f0f0f0\">\n" + //
+            "<h2 align=\"center\">" + title + "</h2>\n" + //
+            "<ul>\n" + //
+
+            "  <li><b>Title</b>: " + booktitle + "\n" + //
+            "  <li><b>Author</b>: " + author + "\n" + //
+            "  <li><b>Publisher</b>: " + pub + "\n" + //
+            "  <li><b>Available</b>: " + available + "\n" + //
+
+            "</ul>\n");
+
+      out.println("<a href=/webproject/simpleFormSearch.html>Search Data</a> <br>");
+      out.println("</body></html>");
+   }
+
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      doGet(request, response);
+   }
+
+}

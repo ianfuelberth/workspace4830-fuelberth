16e5002de10e4ecc4300cca785d618c921cdeb77
ifuelberth <ifuelberth@10.70.251.116>
2023-10-13 22:49:34 -0500

    Final Clean Up

--- webproject-techexercise-fuelberth/src/main/java/SimpleFormInsert.java
+++ webproject-techexercise-fuelberth/src/main/java/SimpleFormInsert.java
@@ -15,106 +15,106 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/SimpleFormInsert")
 public class SimpleFormInsert extends HttpServlet {
-   private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-   public SimpleFormInsert() {
-      super();
-   }
+	public SimpleFormInsert() {
+		super();
+	}
 
-   @Override
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      String booktitle = request.getParameter("title");
-      String author = request.getParameter("author");
-      String pub = request.getParameter("publisher");
-      String[] boxValues = request.getParameterValues("available");
-      String boxValue = boxValues[0];
-      Boolean available = Boolean.parseBoolean(boxValue);
-      
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String booktitle = request.getParameter("title");
+		String author = request.getParameter("author");
+		String pub = request.getParameter("publisher");
+		String[] boxValues = request.getParameterValues("available");
+		String boxValue = boxValues[0];
+		Boolean available = Boolean.parseBoolean(boxValue);
 
-      Connection connection = null;
-      String insertSql = " INSERT INTO libraryTable (id, title, author, publisher, available) values (default, ?, ?, ?, ?)";
 
-      try {
-         DBConnection.getDBConnection();
-         connection = DBConnection.connection;
-         PreparedStatement preparedStmt = connection.prepareStatement(insertSql);
-         preparedStmt.setString(1, booktitle);
-         preparedStmt.setString(2, author);
-         preparedStmt.setString(3, pub);
-         preparedStmt.setBoolean(4, available);
-         preparedStmt.execute();
-         connection.close();
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
+		Connection connection = null;
+		String insertSql = " INSERT INTO libraryTable (id, title, author, publisher, available) values (default, ?, ?, ?, ?)";
 
-      // Set response content type
-      response.setContentType("text/html");
-      PrintWriter out = response.getWriter();
-      String title = "Insert New Book";
-      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+		try {
+			DBConnection.getDBConnection();
+			connection = DBConnection.connection;
+			PreparedStatement preparedStmt = connection.prepareStatement(insertSql);
+			preparedStmt.setString(1, booktitle);
+			preparedStmt.setString(2, author);
+			preparedStmt.setString(3, pub);
+			preparedStmt.setBoolean(4, available);
+			preparedStmt.execute();
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-      out.println(docType + 
-  	    	"<html>\n" + //
-  	  		"<head>\n"
-  	  		+ "<style>\n"
-  	  		+ "header {\n"
-  	  		+ "    background-color:rgb(43, 123, 76);\n"
-  	  		+ "    color:white;\n"
-  	  		+ "    text-align:center;\n"
-  	  		+ "    padding:5px;	 \n"
-  	  		+ "}\n"
-  	  		+ "nav {\n"
-  	  		+ "    line-height:30px;\n"
-  	  		+ "    background-color:#eeeeee;\n"
-  	  		+ "    height:300px;\n"
-  	  		+ "    width:100px;\n"
-  	  		+ "    float:left;\n"
-  	  		+ "    padding:5px;	      \n"
-  	  		+ "}\n"
-  	  		+ "section {\n"
-  	  		+ "    width:350px;\n"
-  	  		+ "    float:left;\n"
-  	  		+ "    padding:10px;	 	 \n"
-  	  		+ "}\n"
-  	  		+ "footer {\n"
-  	  		+ "    background-color:rgb(43, 123, 76);\n"
-  	  		+ "    color:white;\n"
-  	  		+ "    clear:both;\n"
-  	  		+ "    text-align:center;\n"
-  	  		+ "    padding:5px;	 	 \n"
-  	  		+ "}\n"
-  	  		+ "</style>\n"
-  	  		+ "</head>\n");
-  	    
-  	    out.println("<body><header><h1> Search Results </h1></header></body>");
-  	  out.println("<body><nav>\n"
-      		+ "<a href=\"/webproject-techexercise-fuelberth/simpleFormSearch.html\">Search Books</a> <br>\n"
-      		+ "<a href=\"/webproject-techexercise-fuelberth/simpleFormInsert.html\">Add New Book</a> <br>\n"
-      		+ "<a href=\"/webproject-techexercise-fuelberth/ViewAll\">View All</a> <br>\n"
-      		+ "</nav></body>");
-  	    out.println("<body><section>");
-      
-  	    out.println(
-            "<ul>\n" + //
+		// Set response content type
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String title = "Insert New Book";
+		String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+
+		out.println(docType + 
+				"<html>\n" + //
+				"<head>\n"
+				+ "<style>\n"
+				+ "header {\n"
+				+ "    background-color:rgb(43, 123, 76);\n"
+				+ "    color:white;\n"
+				+ "    text-align:center;\n"
+				+ "    padding:5px;	 \n"
+				+ "}\n"
+				+ "nav {\n"
+				+ "    line-height:30px;\n"
+				+ "    background-color:#eeeeee;\n"
+				+ "    height:300px;\n"
+				+ "    width:100px;\n"
+				+ "    float:left;\n"
+				+ "    padding:5px;	      \n"
+				+ "}\n"
+				+ "section {\n"
+				+ "    width:350px;\n"
+				+ "    float:left;\n"
+				+ "    padding:10px;	 	 \n"
+				+ "}\n"
+				+ "footer {\n"
+				+ "    background-color:rgb(43, 123, 76);\n"
+				+ "    color:white;\n"
+				+ "    clear:both;\n"
+				+ "    text-align:center;\n"
+				+ "    padding:5px;	 	 \n"
+				+ "}\n"
+				+ "</style>\n"
+				+ "</head>\n");
+
+		out.println("<body><header><h1> Search Results </h1></header></body>");
+		out.println("<body><nav>\n"
+				+ "<a href=\"/webproject-techexercise-fuelberth/simpleFormSearch.html\">Search Books</a> <br>\n"
+				+ "<a href=\"/webproject-techexercise-fuelberth/simpleFormInsert.html\">Add New Book</a> <br>\n"
+				+ "<a href=\"/webproject-techexercise-fuelberth/ViewAll\">View All</a> <br>\n"
+				+ "</nav></body>");
+		out.println("<body><section>");
+
+		out.println(
+				"<ul>\n" + //
 
             "  <li><b>Title</b>: " + booktitle + "\n" + //
             "  <li><b>Author</b>: " + author + "\n" + //
             "  <li><b>Publisher</b>: " + pub + "\n" + //
             "  <li><b>Available</b>: " + String.valueOf(available) + "\n" + //
 
-            "</ul>\n");
-  	    out.println("</body></section>");
-      
-  	  out.println("<body><footer>\n"
-	       		+ "	Copyright\n"
-	       		+ "</body></footer>");
-      out.println("</html>");
-   }
+				"</ul>\n");
+		out.println("</body></section>");
 
-   @Override
-protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      doGet(request, response);
-   }
+		out.println("<body><footer>\n"
+				+ "	Copyright\n"
+				+ "</body></footer>");
+		out.println("</html>");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 
 }
--- webproject-techexercise-fuelberth/src/main/java/ViewAll.java
+++ webproject-techexercise-fuelberth/src/main/java/ViewAll.java
@@ -14,130 +14,130 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/ViewAll")
 public class ViewAll extends HttpServlet {
-   private static final long serialVersionUID = 1L;
-   static String url = "jdbc:mysql://ec2-3-142-201-14.us-east-2.compute.amazonaws.com:3306/libraryDB";
-   static String user = "ifuel_remote";
-   static String password = "csci4830";
-   static Connection connection = null;
+	private static final long serialVersionUID = 1L;
+	static String url = "jdbc:mysql://ec2-3-142-201-14.us-east-2.compute.amazonaws.com:3306/libraryDB";
+	static String user = "ifuel_remote";
+	static String password = "csci4830";
+	static Connection connection = null;
 
-   public ViewAll() {
-      super();
-   }
+	public ViewAll() {
+		super();
+	}
 
-   @Override
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      display(response);
-   }
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		display(response);
+	}
 
-void display(HttpServletResponse response) throws IOException {
-    response.setContentType("text/html");
-    PrintWriter out = response.getWriter();
-    String title = "All Books";
-    String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + //
-          "transitional//en\">\n"; //
-//    out.println(docType + //
-//          "<html>\n" + //
-//          "<section><title>" + title + "</title></head>\n" + //
-//          "<body bgcolor=\"#f0f0f0\">\n" + //
-//          "<h1 align=\"center\">" + title + "</h1>\n");
-    
-    out.println(docType + 
-    	"<html>\n" + //
-  		"<head>\n"
-  		+ "<style>\n"
-  		+ "header {\n"
-  		+ "    background-color:rgb(43, 123, 76);\n"
-  		+ "    color:white;\n"
-  		+ "    text-align:center;\n"
-  		+ "    padding:5px;	 \n"
-  		+ "}\n"
-  		+ "nav {\n"
-  		+ "    line-height:30px;\n"
-  		+ "    background-color:#eeeeee;\n"
-  		+ "    height:300px;\n"
-  		+ "    width:100px;\n"
-  		+ "    float:left;\n"
-  		+ "    padding:5px;	      \n"
-  		+ "}\n"
-  		+ "section {\n"
-  		+ "    width:350px;\n"
-  		+ "    float:left;\n"
-  		+ "    padding:10px;	 	 \n"
-  		+ "}\n"
-  		+ "footer {\n"
-  		+ "    background-color:rgb(43, 123, 76);\n"
-  		+ "    color:white;\n"
-  		+ "    clear:both;\n"
-  		+ "    text-align:center;\n"
-  		+ "    padding:5px;	 	 \n"
-  		+ "}\n"
-  		+ "</style>\n"
-  		+ "</head>\n");
-    
-    out.println("<body><header><h1> All Books </h1></header></body>");
-    out.println("<body><nav>\n"
-    		+ "<a href=\"/webproject-techexercise-fuelberth/simpleFormSearch.html\">Search Books</a> <br>\n"
-    		+ "<a href=\"/webproject-techexercise-fuelberth/simpleFormInsert.html\">Add New Book</a> <br>\n"
-    		+ "<a href=\"/webproject-techexercise-fuelberth/ViewAll\">View All</a> <br>\n"
-    		+ "</nav></body>");
-    out.println("<body><section>");
-    Connection connection = null;
-    PreparedStatement preparedStatement = null;
-    try {
-       DBConnection.getDBConnection();
-       connection = DBConnection.connection;
+	void display(HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String title = "All Books";
+		String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + //
+				"transitional//en\">\n"; //
+		//    out.println(docType + //
+		//          "<html>\n" + //
+		//          "<section><title>" + title + "</title></head>\n" + //
+		//          "<body bgcolor=\"#f0f0f0\">\n" + //
+		//          "<h1 align=\"center\">" + title + "</h1>\n");
 
-       
-          String selectSQL = "SELECT * FROM libraryTable";
-          preparedStatement = connection.prepareStatement(selectSQL);
-       
-       ResultSet rs = preparedStatement.executeQuery();
+		out.println(docType + 
+				"<html>\n" + //
+				"<head>\n"
+				+ "<style>\n"
+				+ "header {\n"
+				+ "    background-color:rgb(43, 123, 76);\n"
+				+ "    color:white;\n"
+				+ "    text-align:center;\n"
+				+ "    padding:5px;	 \n"
+				+ "}\n"
+				+ "nav {\n"
+				+ "    line-height:30px;\n"
+				+ "    background-color:#eeeeee;\n"
+				+ "    height:300px;\n"
+				+ "    width:100px;\n"
+				+ "    float:left;\n"
+				+ "    padding:5px;	      \n"
+				+ "}\n"
+				+ "section {\n"
+				+ "    width:350px;\n"
+				+ "    float:left;\n"
+				+ "    padding:10px;	 	 \n"
+				+ "}\n"
+				+ "footer {\n"
+				+ "    background-color:rgb(43, 123, 76);\n"
+				+ "    color:white;\n"
+				+ "    clear:both;\n"
+				+ "    text-align:center;\n"
+				+ "    padding:5px;	 	 \n"
+				+ "}\n"
+				+ "</style>\n"
+				+ "</head>\n");
 
-       while (rs.next()) {
-          int id = rs.getInt("id");
-          String name = rs.getString("title").trim();
-          String author = rs.getString("author").trim();
-          String publisher = rs.getString("publisher").trim();
-          String available = String.valueOf(rs.getBoolean("available"));
+		out.println("<body><header><h1> All Books </h1></header></body>");
+		out.println("<body><nav>\n"
+				+ "<a href=\"/webproject-techexercise-fuelberth/simpleFormSearch.html\">Search Books</a> <br>\n"
+				+ "<a href=\"/webproject-techexercise-fuelberth/simpleFormInsert.html\">Add New Book</a> <br>\n"
+				+ "<a href=\"/webproject-techexercise-fuelberth/ViewAll\">View All</a> <br>\n"
+				+ "</nav></body>");
+		out.println("<body><section>");
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		try {
+			DBConnection.getDBConnection();
+			connection = DBConnection.connection;
 
-          out.println("<ul>\n" + //
-          		"  <li><b>Title</b>: " + name + "\n" + //
-          		"  <li><b>Author</b>: " + author + "\n" + //
-          		"  <li><b>Publisher</b>: " + publisher + "\n" + //
-          		"  <li><b>Available</b>: " + String.valueOf(available) + "\n" + //
-	            "</ul>\n");
-          
-       }
-       out.println("</section></body>");
-       
-       out.println("<body><footer>\n"
-       		+ "	Copyright\n"
-       		+ "</body></footer>");
-       out.println("</html>");
-       rs.close();
-       preparedStatement.close();
-       connection.close();
-    } catch (SQLException se) {
-       se.printStackTrace();
-    } catch (Exception e) {
-       e.printStackTrace();
-    } finally {
-       try {
-          if (preparedStatement != null)
-             preparedStatement.close();
-       } catch (SQLException se2) {
-       }
-       try {
-          if (connection != null)
-             connection.close();
-       } catch (SQLException se) {
-          se.printStackTrace();
-       }
-    }
-}
-   
-   @Override
-protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      doGet(request, response);
-   }
+
+			String selectSQL = "SELECT * FROM libraryTable";
+			preparedStatement = connection.prepareStatement(selectSQL);
+
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("title").trim();
+				String author = rs.getString("author").trim();
+				String publisher = rs.getString("publisher").trim();
+				String available = String.valueOf(rs.getBoolean("available"));
+
+				out.println("<ul>\n" + //
+						"  <li><b>Title</b>: " + name + "\n" + //
+						"  <li><b>Author</b>: " + author + "\n" + //
+						"  <li><b>Publisher</b>: " + publisher + "\n" + //
+						"  <li><b>Available</b>: " + String.valueOf(available) + "\n" + //
+						"</ul>\n");
+
+			}
+			out.println("</section></body>");
+
+			out.println("<body><footer>\n"
+					+ "	Copyright\n"
+					+ "</body></footer>");
+			out.println("</html>");
+			rs.close();
+			preparedStatement.close();
+			connection.close();
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} catch (SQLException se2) {
+			}
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
